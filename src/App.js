fa6e8554b35011d537ddc2ac95dab3e3663d46f8
AkinduRamsith <akiyaramsith2002@gmail.com>
2023-09-11 22:06:12 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Home from "./Pages/Home/Home";
 
 
 function App() {
   return (
     <div className="App">
-     
+      <Home/>
     </div>
   );
 }
---
+++ src/components/Navigation/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <div className="navBarBox">
+      
+    </div>
+  )
+}
+
